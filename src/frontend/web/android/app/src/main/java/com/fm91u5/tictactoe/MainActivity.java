131c02011334da7bcfe831a8ca9934c06beca480
Popper Julius Ryunosuke <FM91U5@tr.pte.hu>
2025-09-30 17:56:13 +0200

    Installed Capacitor JS. Android port has been set up.

---
+++ src/frontend/web/android/app/src/main/java/com/fm91u5/tictactoe/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fm91u5.tictactoe;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/frontend/web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fm91u5.tictactoe',
+  appName: 'TicTacToe-FM91U5',
+  webDir: 'dist/web/browser'
+};
+
+export default config;
